9dd026df23f189392df6b56852ec5c1e75d91ff4
Muhammad Rafael Ghifari <business.rafaelghifari@gmail.com>
2023-09-13 11:13:50 +0700

    Update export-personal-computer.ts

--- src/helpers/export-personal-computer.ts
+++ src/helpers/export-personal-computer.ts
@@ -1,15 +1,13 @@
-import * as FileSaver from "file-saver";
-import * as XLSX from "xlsx";
+import { utils, set_fs, writeFile } from "xlsx";
 import * as fs from "fs";
 
 export const ExportExcel = (apiData: unknown[], fileName: string) => {
-	const fileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
 	const fileExtension = ".xlsx";
 
-	XLSX.set_fs(fs);
-	
-	const worksheet = XLSX.utils.json_to_sheet(apiData);
-	const workbook = XLSX.utils.book_new();
-	XLSX.utils.book_append_sheet(workbook, worksheet, "IT Assets");
-	XLSX.writeFile(workbook, fileName + fileExtension, { compression: true });
+	set_fs(fs);
+
+	const worksheet = utils.json_to_sheet(apiData);
+	const workbook = utils.book_new();
+	utils.book_append_sheet(workbook, worksheet, "IT Assets");
+	writeFile(workbook, fileName + fileExtension, { compression: true });
 }
